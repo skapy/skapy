8efe313f1be3dd8dc4950e515b009c3824a412eb
Dariusz Schmidt <dariusz_schmidt@hotmail.com>
2010-10-20 09:07:56 -0600

    2

---
+++ src/skapy.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : skapy.cpp
+// Author      : Dariusz Schmidt
+// Version     :
+// Copyright   : GPL
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
